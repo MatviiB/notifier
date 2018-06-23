804df217c9614acd6c70164490c621e1c49e4c0b
Matvii <matvii.bondar@gmail.com>
2018-06-23 17:03:10 +0300

    commit

--- src/views/assets/vue-notes.asset.js
+++ src/views/assets/vue-notes.asset.js
@@ -4,14 +4,13 @@ window.vueNotification = require('vue-notification').default;
 Vue.use(vueNotification);
 const note = new Vue({el: '#notes'});
 
-if (socket) {
-    socket.addEventListener('message', function (event) {
-        let data = JSON.parse(event.data).data;
+socket.addEventListener('message', function (event) {
+    let data = JSON.parse(event.data).data;
 
-        note.$notify({
-            type: data.type ? data.type : 'success',
-            title: data.title ? data.title : 'Note:',
-            text: data.text ? data.text : ''
-        });
+    note.$notify({
+        type: data.type ? data.type : 'success',
+        title: data.title ? data.title : 'Note:',
+        text: data.text ? data.text : ''
     });
-}
+});
+
