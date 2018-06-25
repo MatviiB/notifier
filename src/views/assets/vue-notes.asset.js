aef404b33a92015b19cbbe6c087e75b813d83f2e
Matvii <matvii.bondar@gmail.com>
2018-06-25 11:25:39 +0300

    ability to split msgs to notes and custom channel

--- src/views/assets/vue-notes.asset.js
+++ src/views/assets/vue-notes.asset.js
@@ -6,11 +6,11 @@ const note = new Vue({el: '#notes'});
 
 socket.addEventListener('message', function (event) {
     let data = JSON.parse(event.data).data;
-
-    note.$notify({
-        type: data.type ? data.type : 'success',
-        title: data.title ? data.title : 'Note:',
-        text: data.text ? data.text : ''
-    });
+    if (data.note) {
+        note.$notify({
+            type: data.type ? data.type : 'success',
+            title: data.title ? data.title : 'Note:',
+            text: data.text ? data.text : ''
+        });
+    }
 });
-
